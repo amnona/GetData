54022d92db682cfdacd9a500386c763d0e19aae7
amnona <amnonim@gmail.com>
2023-12-18 20:53:48 +0200

    update its primers

--- process_experiment.py
+++ process_experiment.py
@@ -283,7 +283,7 @@ def process_experiment(infile, sra_path, reads_dir=None, max_test=10, skip_get=F
 		# primers={'GTAAAAGTCGTAACAAGG': 'ITS5', 'GTAAAAGTCGTAACAAGGTTTC': 'ITS1F', 'TCCGTAGGTGAACCTGCGG': 'ITS1'}
 		# kmers={'ITS5': ['TTTC','TCTC'], 'ITS1F': [], 'ITS1': []}
 		primts = {'GTAAAAGTCGTAACAAGGNNNN': 'ITSF', 'TCCGTAGGTGAACCTGCGG': 'ITS1'}
-		kmers={'ITS5': ['TTTC','TCTC'], 'ITS1F': ['TTTC'], 'ITS1': ['TTTC']}
+		kmers={'ITSF': ['TTTC'], 'ITS1': ['TTTC']}
 		logging.info('its experiment. will for the following regions: %s' % kmers.keys())
 	else:
 		raise ValueError('unknown experiment type %s (use "16s" or "its")' % exp_type)
