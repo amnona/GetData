35f4d82b3c7129407971c517552c09e1d243cda9
amnona <amnonim@gmail.com>
2026-07-31 16:38:16 +0300

    update hpc login server

--- prep_local.sh
+++ prep_local.sh
@@ -32,11 +32,11 @@ cat SraRunTable.txt | tr "," "\\t" > map.txt
 cp ../analysis.ipynb .
 
 # make remote directory
-ssh amnonam@sheshet.cslab.openu.ac.il "ssh login8.hpc.pub.lan mkdir work/$DIR_NAME"
+ssh amnonam@sheshet.cslab.openu.ac.il "ssh login.hpc.pub.lan mkdir work/$DIR_NAME"
 # ssh amnonam@sheshet.cslab.openu.ac.il "ssh my.hpc.pub.lan mkdir work/$DIR_NAME"
 
 # copy the SraRunTable to the openu server
-rsync -avzP -e 'ssh -o "ProxyCommand ssh amnonam@sheshet.cslab.openu.ac.il -W %h:%p"' SraRunTable.txt amnonam@login8.hpc.pub.lan:work/$DIR_NAME/
+rsync -avzP -e 'ssh -o "ProxyCommand ssh amnonam@sheshet.cslab.openu.ac.il -W %h:%p"' SraRunTable.txt amnonam@login.hpc.pub.lan:work/$DIR_NAME/
 # copy to sheshet
 # rsync -avzP SraRunTable.txt syonat4@sheshet.cslab.openu.ac.il:SraRunTable.txt
 # ssh syonat4@sheshet.cslab.openu.ac.il "rsync -avzP SraRunTable.txt my.hpc.pub.lan:work/$1/"
@@ -47,4 +47,4 @@ rsync -avzP -e 'ssh -o "ProxyCommand ssh amnonam@sheshet.cslab.openu.ac.il -W %h
 # ssh amnonam@shetshet.cslab.openu.ac.il "ssh my.hpc.pub.lan \"cd work/$1 ; /home/amnonam/scripts/doit2.sh --mem-per-cpu=32000 --job-name=$1 --mail-type=end --mail-user=amnonim@gmail.com\""
 
 # ssh amnonam@sheshet.cslab.openu.ac.il "ssh login8.hpc.pub.lan \"cd work/$DIR_NAME ; /home/amnonam/scripts/doit2.sh --job-name=$DIR_NAME --mail-type=end --mail-user=amnonim@gmail.com\"" 
-ssh amnonam@sheshet.cslab.openu.ac.il "ssh login8.hpc.pub.lan \"cd work/$DIR_NAME ; /home/amnonam/scripts/doit2.sh $DIR_NAME $@\"" 
+ssh amnonam@sheshet.cslab.openu.ac.il "ssh login.hpc.pub.lan \"cd work/$DIR_NAME ; /home/amnonam/scripts/doit2.sh $DIR_NAME $@\"" 
