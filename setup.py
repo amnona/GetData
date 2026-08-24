25491d388666627db4289d45b73d1a2e1f3e90a2
amnona <amnonim@gmail.com>
2026-08-24 17:33:41 +0300

    bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ description = 'Downloading 16S data from SRA using Deblur'
 
 setup(
     name='GetData',
-    version='0.1.3',
+    version='0.1.4',
     description=description,
     long_description=description,
     long_description_content_type='text/markdown',
